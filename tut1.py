69470504c8af5cfda94d951419aaac08b05c4a3f
kacka or kasked <nov.sofiya@gmail.com>
2025-02-09 19:24:10 +0200

    tutorial

---
+++ tut1.py
@@ -0,0 +1,16 @@
+# функция print
+print("Hello world")
+#Переменные
+test_num = 1
+test_text = "subscribe :3"
+print(test_num, test_text)
+#f строка
+test_num = 1
+test_text = "subscribe :3"
+print(f"сабнись пж: {test_num} {test_text}")
+import time
+print("Загрузка...")
+time.sleep(5)
+print("Загрузка завершена!")
+
+# by https://www.youtube.com/@kaskedofficial
